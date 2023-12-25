d93c58393d4cf4dc8421e23845b409a198abb6ef
Agent-75 <abbass.babass10@gmail.com>
2023-12-25 06:15:37 +0200

    implemented a method in /access-tokens endpoint to handle user's sudden country change

--- app/Http/Controllers/AccessTokenController.php
+++ app/Http/Controllers/AccessTokenController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Enums\TokenAbility;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Laravel\Sanctum\PersonalAccessToken;
+use Stevebauman\Location\Facades\Location;
 
 use function PHPUnit\Framework\isEmpty;
 
 class AccessTokenController extends Controller
 {
-    public function isNewestRefreshToken($plainTextRefreshToken){
+    protected function isNewestRefreshToken($plainTextRefreshToken){
         $refresh_token = PersonalAccessToken::findToken($plainTextRefreshToken);
         $user = $refresh_token->tokenable;
 
@@ -30,20 +33,37 @@ class AccessTokenController extends Controller
         return true;   
     }
 
+    //compare user's location with the previous user's location
+    public function isFromDifferentCountry($request){
+        $ip = $request->getClientIp();
+        $current_user_location = Location::get($ip);
+        if (!$current_user_location) return false;
+        
+        $current_user_country = $current_user_location->countryName;
+        $previous_user_country = session()->get('user_location_country',null);
+
+        // update the previous location to current
+        session()->put('user_location_country', $current_user_country);
+
+        if (!$previous_user_country) return false;
+        return $previous_user_country != $current_user_country;
+    }
+    
     // get a new access token by providing refresh token
     public function createAccessToken(Request $request){
         $user = $request->user();
         $current_refresh_token = $request->bearerToken();
 
+        // if the user suddenly changes countries, the user is malicious
+        if ($this->isFromDifferentCountry($request)){
+            $user->tokens()->delete();
+            throw new AuthenticationException();
+        }
+
         // if the refresh token is not the newest, the user is malicious
         if (!$this->isNewestRefreshToken($current_refresh_token)){
             $user->tokens()->delete();
-            $error = [
-                'message' => 'Unauthenticated.',
-                'code' => 401,
-            ];
-            $response_body = HelperController::getFailedResponse($error, null);
-            return response($response_body, 401);
+            throw new AuthenticationException();
         } 
 
         $token_operations = function () use($user){
